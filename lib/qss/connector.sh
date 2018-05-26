b889d4fcd1dea2ca7516a68d8b895b6bff45dff9
Samuel Lair <lair001@gmail.com>
2018-05-26 02:18:55 -0400

    Remove redundant host address check before sshing
    Rendered redundant by host address validator.

--- lib/qss/connector.sh
+++ lib/qss/connector.sh
@@ -2,11 +2,5 @@ ssh_into_server() {
 	validateUsername "$username"
 	validate_host_address "$host_address"
 	validateKeyFile $(get_key_file_path)
-	if [ ! -z "$host_address" ]; then
-		ssh "$username"'@'"$host_address" -i $(get_key_file_path)
-		exit 0
-	else
-		echo "Cannot SSH. No ip address loaded."
-		exit 2
-	fi
+	ssh "$username"'@'"$host_address" -i $(get_key_file_path)
 }
