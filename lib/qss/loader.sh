9d33bf9cdf0bd95287c0251691ad01895c4cb004
Samuel Lair <lair001@gmail.com>
2018-05-27 01:32:24 -0400

    Read default configuration instead of sourcing it

--- lib/qss/loader.sh
+++ lib/qss/loader.sh
@@ -1,9 +1,12 @@
 load_configuration() {
 	if [ $config_file_path ] && [ -f $config_file_path ]; then
-		source $config_file_path
-		keys_directory_path=$default_keys_directory_path
-		profiles_directory_path=$default_profiles_directory_path
-		profile_file_name=$default_profile_file_name
+		default_keys_directory_path=$(read_default_keys_directory_path "$config_file_path")
+		default_profiles_directory_path=$(read_default_profiles_directory_path "$config_file_path")
+		default_profile_file_name=$(read_default_profile_file_name "$config_file_path")
+
+		keys_directory_path="$default_keys_directory_path"
+		profiles_directory_path="$default_profiles_directory_path"
+		profile_file_name="$default_profile_file_name"
 	fi
 }
 
@@ -11,9 +14,8 @@ load_profile() {
 	if [ ! -z "$1" ]; then
 		profile_file_name="$1"
 	fi
-	local profile_file_path=$(get_profile_file_path)
 
-	validate_profile_file "$profile_file_path"
+	local profile_file_path=$(get_profile_file_path)
 
 	default_username=$(read_default_username "$profile_file_path")
 	default_host_address=$(read_default_host_address "$profile_file_path")
