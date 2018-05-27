81f1356848355a7db4890d74ce1a50c634b69f96
Samuel Lair <lair001@gmail.com>
2018-05-27 01:18:27 -0400

    Implement configuration file reader
    Also renamed reader to profile file reader.

---
+++ lib/qss/configuration-file-reader.sh
@@ -0,0 +1,11 @@
+read_default_keys_directory_path() {
+	printf $(cat "$1" | grep 'default_keys_directory_path=' | sed -r 's|default_keys_directory_path=||' | sed -r 's:~|\$HOME:'"$HOME"':')
+}
+
+read_default_profiles_directory_path() {
+	printf $(cat "$1" | grep 'default_profiles_directory_path=' | sed -r 's|default_profiles_directory_path=||' | sed -r 's:~|\$HOME:'"$HOME"':')
+}
+
+read_default_profile_file_name() {
+	printf $(cat "$1" | grep 'default_profile_file_name=' | sed -r 's|default_profile_file_name=||')
+}
