11b02e1d647ab14d540e5e3d554b557622090dba
Samuel Lair <lair001@gmail.com>
2018-05-28 18:39:30 -0400

    Standardize designation of defaults

--- lib/qss/defaults-manager.sh
+++ lib/qss/defaults-manager.sh
@@ -1,15 +1,24 @@
+designate_default() {
+	local field_name="$1"
+	local new_field_value="$2"
+	local path_for_file_containing_default_for_field="$3"
+
+	local name_of_default_value_for_field="default_$field_name"
+	sed -i "s|$name_of_default_value_for_field="${!name_of_default_value_for_field}"|$name_of_default_value_for_field=$new_field_value|g" "$path_for_file_containing_default_for_field"
+}
+
 designate_default_username() {
-	sed -i "s/default_username=$default_username/default_username=$1/g" $(get_profile_file_path)
+	designate_default 'username' "$1" $(get_profile_file_path)
 }
 
 designate_default_host_address() {
-	sed -i "s/default_host_address=$default_host_address/default_host_address=$1/g" $(get_profile_file_path)
+	designate_default 'host_address' "$1" $(get_profile_file_path)
 }
 
 designate_default_key_file() {
-	sed -i "s|default_key_file_name=$default_key_file_name|default_key_file_name=$1|g" $(get_profile_file_path)
+	designate_default 'key_file_name' "$1" $(get_profile_file_path)
 }
 
 designate_default_profile_file() {
-	sed -i "s|default_profile_file_name=$default_profile_file_name|default_profile_file_name=$1|g" $config_file_path
+	designate_default 'profile_file_name' "$1" "$config_file_path"
 }
